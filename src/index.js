a46301ca8a5da99549d939f44944d0aef3b8985f
HovhannesWA <hovhannes.keshishyan@willing-able.dev>
2022-07-26 23:04:30 +0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { Observable } from 'rxjs';
+
+const observable = new Observable(subscriber => {
+  subscriber.next(1);
+  subscriber.next(2);
+  subscriber.next(3);
+  setTimeout(() => {
+    subscriber.next(4);
+    subscriber.complete();
+  }, 1000);
+});
+
+observable.subscribe(v => console.log(v));
